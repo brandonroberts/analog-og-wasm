08e3cd528d5e91978bd251fe41034b7c3fcbf164
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-25 18:49:12 -0600

    cloudflare maybe?

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,10 @@ export default defineConfig(() => ({
       },
       nitro: {
         preset: 'vercel-edge',
+        cloudflare: {
+          deployConfig: true,
+          nodeCompat: true
+        },
         rollupConfig: {
           external: [/^@cf-wasm\/.*/],
         },
